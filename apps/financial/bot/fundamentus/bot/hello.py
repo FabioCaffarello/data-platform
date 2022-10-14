f32115cac0b1c5d74addc5d465c3acab6d458469
FabioCaffarello <fabio.caffarello@gmail.com>
2022-10-14 20:42:51 -0300

    feat: add api docs to ci

--- apps/financial/bot/fundamentus/bot/hello.py
+++ apps/financial/bot/fundamentus/bot/hello.py
@@ -1,3 +1,5 @@
+"""Hello module."""
+
 
 def hello():
     """Hello."""
--- apps/financial/bot/fundamentus/tests/test_hello.py
+++ apps/financial/bot/fundamentus/tests/test_hello.py
@@ -1,5 +1,8 @@
+"""bot.hello tests."""
+
 from bot import hello
 
 
 def test_index():
+    """Hello Tests."""
     assert hello.hello() == "Hello financial-bot-fundamentus"
